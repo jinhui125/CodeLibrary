ee458a6dd7d534c6a73c95c48dc737f5f18f4c39
jinhui125 <2594401145@qq.com>
2025-04-28 00:22:42 +0800

    新增comTools内容

--- comTools_DEV/v2.0/common_func.py
+++ comTools_DEV/v2.0/common_func.py
@@ -63,7 +63,35 @@ class CommonFunc:
         subprocess_obj.wait()
         sys.exit(0)
 
+    def string_format(self, string, way, width, fill=' '):
+
+        count = 0
+        for word in string:
+            if (word >= '\u4e00' and word <= '\u9fa5') or word in ['；', '：', '，', '（', '）', '！', '？', '——', '……',
+                                                                '、', '》', '《']:
+                count += 1
+        width = width-count if width >= count else 0
+        return f'{string:{fill}{way}{width}}'
+
+    def table(self, data_ls: list, width=50):
+
+        row_dividing_line = '-' * width
+        content_ls = []
+        for data in data_ls:
+            tmp_ls = []
+            for value in data:
+                tmp_ls.append(self.string_format(string=value, way='^', width=width))
+            content_ls.append(tmp_ls)
+        print(f'-{row_dividing_line}-{row_dividing_line}-')
+        for i in content_ls:
+            print(f'|{i[0]}|{i[1]}|')
+            print(f'-{row_dividing_line}-{row_dividing_line}-')
+
 if __name__ == '__main__':
     test = CommonFunc()
-    test_subprocess = subprocess.Popen(args=f'python ../test_file.py', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    test.handle_signal(subprocess_obj=test_subprocess, text_description='事件模拟中')
+    # test_subprocess = subprocess.Popen(args=f'python ../test_file.py', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # test.handle_signal(subprocess_obj=test_subprocess, text_description='事件模拟中')
+    # result = test.string_format(string='请在下面云舱代驾环境选项中选择pre', way='^', width=100, fill='-')
+    result = test.table(data_ls=[['QA测试环境', 'pre'], ['线上运营环境', 'online'], ['opn环境', 'opn']], width=30)
+    # print(f'{result}')
+
