cda743bf33bc9b627666f3c9d61766763b3a292a
jinhui125 <2594401145@qq.com>
2025-05-19 20:55:54 +0800

    提交修改

---
+++ practice-example/javascript/test.js
@@ -0,0 +1,22 @@
+let num = 123;
+let str = 'Hello';
+let bool = true;
+let nothing = null;
+let notDefine = undefined;
+let sym = Symbol('id');
+let bigNum = 123n;
+
+let obj = { name: '张三'};
+let arr = [1, 2, 3];
+let func = function() {};
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof nothing);
+console.log(typeof notDefine);
+console.log(typeof sym);
+console.log(typeof bigNum);
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof func);
